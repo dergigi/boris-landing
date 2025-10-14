27951b40c65abd389398c0cf60d3d8002855793f
Gigi <dergigi@pm.me>
2025-10-14 20:48:17 +0200

    chore: set up ESLint and TypeScript checking for code quality
    - Install eslint with astro plugin and typescript support
    - Add lint and type-check scripts to package.json
    - Configure eslint.config.js with recommended Astro rules
    - Install @astrojs/check for Astro type checking
    - All checks pass with 0 errors

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+
+export default [
+  ...eslintPluginAstro.configs.recommended,
+  {
+    rules: {
+      // Custom rules can be added here
+    }
+  }
+];
+
